6e173abe8cdcd182a40e05b4036394701183d41c
Jim Eckerlein <jim.eckerlein@icloud.com>
2021-07-17 22:26:05 +0200

    prefix functions with minima_

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,28 +1,10 @@
-function one_pwd
-    echo $PWD | sed 's:/Users/'$USER':~:'
-end
-
-function relpath
-    set -l pos $argv[1]
-    set -l ref $argv[2]
-    set -l down ''
-
-    # case "$ref" in $pos/*) break;; esac
-
-    while true
-        if [ $pos = '/' ]
-            break
-        end
-    end
-end
-
-function path_to_array
+function minima_path_to_array
     echo $argv[1] | sed 's:/:\n:g'
 end
 
-function shortened_path
-    set long (path_to_array $PWD)
-    set short (path_to_array $argv[1])
+function minima_shortened_path
+    set long (minima_path_to_array $PWD)
+    set short (minima_path_to_array $argv[1])
     set start (count $short)
     set end (count $long)
     for i in (seq $start $end)
@@ -33,34 +15,14 @@ function shortened_path
     end
 end
 
-function pwd
+function minima_pwd
     set repos_root (git rev-parse --show-toplevel 2> /dev/null)
     if [ $status = 0 ]
-        set dir (shortened_path $repos_root)
+        set dir (minima_shortened_path $repos_root)
     else
         set dir $PWD
     end
     echo $dir | sed -E 's:^/Users/'$USER':~:'
-end
-
-function git_status_icon
-    if echo $argv[1] | grep -q -e $argv[3]
-        set_color $argv[2]
-        echo -n ' '$argv[3]
-        set_color white
-    end
-end
-
-function git_status_icons
-    set -l git_status (echo (git status --porcelain 2> /dev/null | sed -E 's/(^.{3}).*/\1/' | tr -d ' \n'))
-
-    git_status_icon $git_status red 'D' # Deleted
-    git_status_icon $git_status yellow 'R' # Renamed
-    git_status_icon $git_status yellow 'C' # Copied
-    git_status_icon $git_status green 'A' # Added
-    git_status_icon $git_status blue 'U' # Updated but unmerged
-    git_status_icon $git_status yellow 'M' # Modified
-    git_status_icon $git_status yellow '?' # Untracked
 end
 
 function fish_prompt
@@ -80,7 +42,7 @@ function fish_prompt
     end
     set_color white
 
-    echo -n (pwd)
+    echo -n (minima_pwd)
 
     tput sgr0
     echo -n '  '
